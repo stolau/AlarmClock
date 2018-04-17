f17837f305fd28daab7f114792643f3acae7bef8
tp-sy <36451441+tp-sy@users.noreply.github.com>
2018-04-17 10:44:04 +0300

    Update IRRecieve.ino

--- IRRecieve.ino
+++ IRRecieve.ino
@@ -1,11 +1,3 @@
-/*
- * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
- * An IR detector/demodulator must be connected to the input RECV_PIN.
- * Version 0.1 July, 2009
- * Copyright 2009 Ken Shirriff
- * http://arcfn.com
- */
-
 #include <IRremote.h>
 
 int RECV_PIN = 11;
@@ -17,17 +9,15 @@ decode_results results;
 void setup()
 {
   Serial.begin(9600);
-  // In case the interrupt driver crashes on setup, give a clue
-  // to the user what's going on.
   Serial.println("Enabling IRin");
-  irrecv.enableIRIn(); // Start the receiver
+  irrecv.enableIRIn();
   Serial.println("Enabled IRin");
 }
 
 void loop() {
   if (irrecv.decode(&results)) {
     Serial.println(results.value, HEX);
-    irrecv.resume(); // Receive the next value
+    irrecv.resume();
   }
   delay(100);
 }
